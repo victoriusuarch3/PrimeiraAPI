cec2aba329e0f035fb19036d5dce3681f0d631c8
Víctor Vitoriano <victorfrance20@yahoo.com>
2022-05-06 23:08:25 -0300

    criando api

--- PrimeiraAPI.Web/Controllers/PrimeiraController.cs
+++ PrimeiraAPI.Web/Controllers/PrimeiraController.cs
@@ -3,7 +3,7 @@ namespace PrimeiraAPI.Web.Controllers
 {
     [ApiController]
     [Route("MinhaController")]
-    public class PrimeiraController
+    public class PrimeiraController : ControllerBase
     {
         [HttpGet]
         public string PrimeiraAPI(){
@@ -14,6 +14,14 @@ namespace PrimeiraAPI.Web.Controllers
         [HttpGet("segundaAPI")]
         public string SegundaAPI(){
             return "Teste";
+        }
+
+        [HttpGet("TerceiraAPI")]
+        public IActionResult OutraAPI(int entrada){
+            if(entrada == 1){
+                return BadRequest("1 não é uma entrada válida");
+            }
+            return Ok("Dado inserido foi:" + entrada);
         }
     }
 }
